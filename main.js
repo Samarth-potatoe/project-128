f8451922db1aa6229d4e3546e0997004be6e2ff5
Samarth-potatoe <83120604+Samarth-potatoe@users.noreply.github.com>
2022-07-14 21:45:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+peter_pan_song = "";
+harry_potter_song = "";
+leftWrist_x = 0;
+leftWrist_y = 0;
+rightWrist_x = 0;
+rightWrist_y = 0;
+
+
+function setup()
+{
+    canvas = createCanvas(600, 530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    //poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    Image(video, 0, 0, 600, 530);
+}
+
+function preload()
+{
+    peter_pan_song = loadSound("music2.mp3");
+    harry_potter_song = loadSound("music.mp3");
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = " + leftWrist_x + "leftWrist_y = " + leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = " + rightWrist_x + "rightWrist_y = " + rightWrist_y);
+    }
+}
